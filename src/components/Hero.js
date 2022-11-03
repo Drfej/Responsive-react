7a3e139d73857c4966eb3b6f4124777d3aae4832
Funsho <fejjy1998@gmail.com>
2022-11-03 21:03:08 +0100

    my first responsive code commit

---
+++ src/components/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='content'>
+            <p>Call us</p>
+            <p>+123 9055427727</p>
+            <p>Because Money</p>
+            <p>Doesn't Come With Instructions</p>
+            <button href='/' className='button'>Free Consultation</button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
